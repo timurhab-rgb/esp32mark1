b816cf99786c935b694b7184702fbc54c1b7219e
timurhab-rgb <timurhab@icloud.com>
2026-03-20 19:33:20 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py - файл с настройками WiFi и токеном бота
 
 wifi_config = {
-    "ssid": "",
-    "password": ""          
+    "ssid": "Vertikal463_2.4G",
+    "password": "18071712"          
 }
 
 utelegram_config = {
-    "token": ""  
+    "token": "8561809211:AAFkmoZ9bAhJ4TOj7Z7tg7Z2G8zcS-L7MPw"
 }
